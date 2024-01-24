802efb937f7b096ad232c59db1cde5a5901a08dd
AdityaSP <sp.aditya@gmail.com>
2024-01-25 02:51:00 +0530

    Fetch default VPC

--- bin/cdkprojuser1.ts
+++ bin/cdkprojuser1.ts
@@ -4,4 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Cdkprojuser1Stack } from '../lib/cdkprojuser1-stack';
 
 const app = new cdk.App();
-new Cdkprojuser1Stack(app, 'Cdkprojuser1Stack');
+const myenv = {account: '008239258920', region: 'us-east-1'}
+new Cdkprojuser1Stack(app, 'Cdkprojuser1Stack', {
+    env: myenv
+});
--- lib/cdkprojuser1-stack.ts
+++ lib/cdkprojuser1-stack.ts
@@ -3,52 +3,64 @@ import { Bucket } from "aws-cdk-lib/aws-s3";
 
 import { Construct } from 'constructs';
 import { LogStorageBucket } from './constructs/logstoragebucket';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class Cdkprojuser1Stack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 
     super(scope, id, props);
-    // const ls = new LogStorageBucket(this, 'MyProjectLogs');
+
+    const existingDefaultVpc = Vpc.fromLookup(this, 'default', {
+      isDefault: true
+    })
+
+    new cdk.CfnOutput(this, 'DefaultVPCId', {
+      value: existingDefaultVpc.vpcId
+    })
+
     
-    const maxObjSizeParam = new cdk.CfnParameter(this, 'maxObjSize',{
-      default : 1024,
-      minValue : 512,
-      maxValue : 2048,
-      type: 'Number'
-    }) 
+    // const ls = new LogStorageBucket(this, 'MyProjectLogs');
 
-    const expiryDuration = new cdk.CfnParameter(this, 'expiryDuration', {
-      minValue : 90,
-      maxValue : 200,
-      type: 'Number',
-      default : 120
-    })
+    
+  //   const maxObjSizeParam = new cdk.CfnParameter(this, 'maxObjSize',{
+  //     default : 1024,
+  //     minValue : 512,
+  //     maxValue : 2048,
+  //     type: 'Number'
+  //   }) 
 
-      const bucketNameParam = new cdk.CfnParameter(this, 'bucketName', {
-        type: 'String',
-        minLength: 5,
-        maxLength: 20,
-        default: "mybucketUser1_2"
-      })
+  //   const expiryDuration = new cdk.CfnParameter(this, 'expiryDuration', {
+  //     minValue : 90,
+  //     maxValue : 200,
+  //     type: 'Number',
+  //     default : 120
+  //   })
 
-   const buck =  new Bucket(this, "bucketIdwithParamName" , {
-      bucketName : bucketNameParam.valueAsString,
-      lifecycleRules : [
-        {
-          expiration : cdk.Duration.days(expiryDuration.valueAsNumber)
-        },
-        {
-          objectSizeGreaterThan: maxObjSizeParam.valueAsNumber,
-          expiration : cdk.Duration.days(3)
+  //     const bucketNameParam = new cdk.CfnParameter(this, 'bucketName', {
+  //       type: 'String',
+  //       minLength: 5,
+  //       maxLength: 20,
+  //       default: "mybucketUser1_2"
+  //     })
+
+  //  const buck =  new Bucket(this, "bucketIdwithParamName" , {
+  //     bucketName : bucketNameParam.valueAsString,
+  //     lifecycleRules : [
+  //       {
+  //         expiration : cdk.Duration.days(expiryDuration.valueAsNumber)
+  //       },
+  //       {
+  //         objectSizeGreaterThan: maxObjSizeParam.valueAsNumber,
+  //         expiration : cdk.Duration.days(3)
           
-        }
-      ]
-  });
+  //       }
+  //     ]
+  // });
 
-    new cdk.CfnOutput(this, 'BucketNameOfmybucketUser1', {
-      value: buck.bucketName
-    })
+  //   new cdk.CfnOutput(this, 'BucketNameOfmybucketUser1', {
+  //     value: buck.bucketName
+  //   })
 
   }
 }
