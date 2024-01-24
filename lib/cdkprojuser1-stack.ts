529c51db54d263cb0de15c7ce6c4a3ed5bc3efd8
AdityaSP <sp.aditya@gmail.com>
2024-01-25 00:46:18 +0530

    Capture output

--- lib/cdkprojuser1-stack.ts
+++ lib/cdkprojuser1-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from "aws-cdk-lib/aws-s3";
+
 import { Construct } from 'constructs';
 import { LogStorageBucket } from './constructs/logstoragebucket';
 
@@ -7,7 +9,24 @@ export class Cdkprojuser1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 
     super(scope, id, props);
-    new LogStorageBucket(this, 'MyProjectLogs');
+    // const ls = new LogStorageBucket(this, 'MyProjectLogs');
+    
+   const buck =  new Bucket(this, "mybucketUser1" , {
+      lifecycleRules : [
+        {
+          expiration : cdk.Duration.days(2)
+        },
+        {
+          objectSizeGreaterThan: 128,
+          expiration : cdk.Duration.days(3)
+          
+        }
+      ]
+  });
+
+    new cdk.CfnOutput(this, 'BucketId', {
+      value: buck.bucketName
+    })
 
   }
 }
