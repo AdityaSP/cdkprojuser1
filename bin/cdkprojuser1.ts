f7003dea21bdf2cf51d8ddbb32540917eefb2151
AdityaSP <sp.aditya@gmail.com>
2024-01-25 03:06:01 +0530

    Default env account and region

--- bin/cdkprojuser1.ts
+++ bin/cdkprojuser1.ts
@@ -4,7 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 import { Cdkprojuser1Stack } from '../lib/cdkprojuser1-stack';
 
 const app = new cdk.App();
-const myenv = {account: '008239258920', region: 'us-east-1'}
+//const myenv = {account: '008239258920', region: 'us-east-1'}
 new Cdkprojuser1Stack(app, 'Cdkprojuser1Stack', {
-    env: myenv
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
 });
